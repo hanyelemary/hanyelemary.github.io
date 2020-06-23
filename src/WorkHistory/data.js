1b778c50ec213db2eb64df905de019ab419eb270
Hany Elemary <hany.elemary@gmail.com>
2020-06-22 20:52:41 -0500

    docs(workhistory/data.js): reword work history to make it less verbose

--- src/WorkHistory/data.js
+++ src/WorkHistory/data.js
@@ -3,31 +3,31 @@ const data = {
     src: './static/images/tw-logo.svg',
     time: 'Nov 2015 - Current',
     title: 'Lead Consultant/Engineer',
-    content: 'ThoughtWorks is my current home. It\'s where I enjoy a healthy dose of tech and thought leadership to deliver sustainable software. At ThoughtWorks, my learning has accelerated by working with talented colleagues in different domains.'
+    content: 'ThoughtWorks is my current home. It\'s where I enjoy a healthy dose of technical leadership, consulting and hands-on development to deliver sustainable software.'
   },
   {
     src: './static/images/oclc-logo.png',
     time: 'May 2011 - September 2015',
     title: 'Software Engineer / Sr. Software Engineer',
-    content: 'At OCLC, I worked on large software projects in terms of scale and impact. I learned about scalability, translation, testability, and most importantly, clean architecture. This is where I adopted lots of engineering best practices by virtue of working with great developers and mentors.'
+    content: 'At OCLC, I worked on large software platforms impacting millions of people around the world. The mission was to spread knowledge through the discoverability of library materials and academic research.'
   },
   {
     src: './static/images/profound-logo.png',
     time: 'November 2007 - April 2011',
     title: 'Application Developer',
-    content: 'At Profound Logic, my focus shifted towards product-thinking. I worked with talented engineers to design, develop and ship software to other developers. Due to our small footprint, I was lucky enough to wear different hats and learn new skills in the fields of Sales and Marketing.'
+    content: 'At Profound Logic, technical product-thinking was my focus. I worked with talented engineers to design, develop and ship platforms to other engineers. Due to our small footprint, I was lucky enough to wear different hats and learn new skills in Sales and Marketing.'
   },
   {
     src: './static/images/lexisnexis-logo.png',
     time: 'March 2006 - November 2007',
     title: 'Capacity and Performance Engineer/Intern',
-    content: 'At LexisNexis, I learned about application performance/monitoring at scale. I wrote visualizations over millions of records for search response times and other perf stats. This, in turn, helped pinpoint bottlenecks in the system.'
+    content: 'At LexisNexis, I worked on application performance/monitoring at scale. I wrote visualizations over millions of records for search response times and other performance and domain stats.'
   },
   {
     src: './static/images/emtec-logo.jpeg',
     time: 'December 2005 - November 2007',
     title: 'Fuel Cell Supply Chain DB Management/Development',
-    content: 'I started my software development experience with an internship at EMTEC. During the internship, I designed & implemented data-entry UIs for the Fuel Cell Supply Chain in addition to managing and designing relational databases.'
+    content: 'I started my professional software development journey here. At EMTEC, I designed & implemented data-entry UIs for the Fuel Cell Supply Chain in addition to designing and managing relational databases.'
   }]
 }
 
